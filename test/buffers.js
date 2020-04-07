48cd0f18ff222769f6ba8a1ea744901740cd396a
Paul.Jones <paul.jones@creditplus.co.uk>
2020-04-07 19:44:36 +0100

    wired up unit tests

--- test/buffers.js
+++ test/buffers.js
@@ -1,12 +1,12 @@
 const expect = require('chai').expect;
-const ne14 = require('../dist/index');
+const ne_text = require('../dist/ne14_text.umd.min');
 
 describe('#buffers', () => {
 
     it('should roundtrip text and buffer', () => {
         let testString = 'hello world',
-            buffer = ne14.Text.textToBuffer(testString),
-            roundTrip = ne14.Text.bufferToText(buffer);
+            buffer = ne_text.textToBuffer(testString),
+            roundTrip = ne_text.bufferToText(buffer);
         expect(roundTrip).to.equal(testString);
     });
 
--- test/format.js
+++ test/format.js
@@ -1,20 +1,20 @@
 const expect = require('chai').expect;
-const ne14 = require('../dist/index');
+const ne_text = require('../dist/ne14_text.umd.min');
 
 describe('#format', () => {
 
     it('should trim a trailing character', () => {
-        var sut = ne14.Text.trimEnd('hello world', 'd');
+        var sut = ne_text.trimEnd('hello world', 'd');
         expect(sut).to.equal('hello worl');
     });
 
     it('should trim multiple leading characters', () => {
-        var sut = ne14.Text.trimStart('xyzworld', 'xyz');
+        var sut = ne_text.trimStart('xyzworld', 'xyz');
         expect(sut).to.equal('world');
     });
 
     it('should trim special characters from either end', () => {
-        var sut = ne14.Text.trimBoth('!{}inner-test!{}', '!{}');
+        var sut = ne_text.trimBoth('!{}inner-test!{}', '!{}');
         expect(sut).to.equal('inner-test');
     });
 });
